14c8f5eb7c177c6c8fe73ebba63a5899523a7220
Jason Ephraim <jason.ephraim.novatex@gmail.com>
2021-01-16 21:07:19 -0600

    fix history caps

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import { Router } from 'react-router-dom';
 
 import './index.scss';
-import history from './utils/History';
+import history from './utils/history';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
